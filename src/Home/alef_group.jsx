364078eb09709d56be175eb9372946064686a74a
Rupesh kumar <rupesh.kumar.light@gmail.com>
2024-02-24 09:18:19 -0800

    update

--- src/Home/alef_group.jsx
+++ src/Home/alef_group.jsx
@@ -5,7 +5,6 @@ const Alef=()=>{
             <div className="container p-5">
                 <h2 className="text-center pb-5">About Alef Group</h2>
                 <p>Alef Group</p>
-                <h2>Rupesh kumar</h2>
             </div>
         </div>
         </>
